b10aecfa787e582bb9d8673341197776279f2038
nfanaras <nfanaras@umich.edu>
2019-12-06 13:36:54 -0500

    database works!!

--- json-to-db.py
+++ json-to-db.py
@@ -4,20 +4,11 @@ import json
 
 # reading data from restaurant json file
 
-full_path = os.path.join(os.path.dirname(__file__), "yelp-resaurant-data.json")
-f = open(full_path)
+full_path = os.path.join(os.path.dirname(__file__), "yelp-restaurant-data.json")
+f = open(full_path, encoding="utf8")
 rest_data = f.read()
 f.close()
 json_rest_data = json.loads(rest_data) # now a dictionary
-
-
-# reading data from hotel json file
-
-full_path = os.path.join(os.path.dirname(__file__), "yelp-hotel-data.json")
-f = open(full_path)
-hotel_data = f.read()
-f.close()
-json_hotel_data = json.loads(hotel_data) # now a dictionary
 
 
 # setting up database
@@ -26,32 +17,52 @@ path = os.path.dirname(os.path.abspath(__file__))
 conn = sqlite3.connect(path+'/'+"final.db")
 cur = conn.cursor()
 
-# creating table for cities with top 5 restaurants and average rating and price
-cur.execute("DROP TABLE IF EXISTS `Top 5 Restaurant Results by Location`")
-cur.execute("CREATE TABLE `Top 5 Restaurant Results by Location` (cities TEXT PRIMARY KEY, #1 TEXT, #2 TEXT, #3 TEXT, #4 TEXT, #5 TEXT, `Average Rating` INTEGER, `Average Price` TEXT)")
+# creating table for cities with top 3 restaurants and ratings
+cur.execute("DROP TABLE IF EXISTS `Restaurants and Ratings`")
+cur.execute("CREATE TABLE `Restaurants and Ratings` (cities TEXT PRIMARY KEY, restaurants varchar(3), ratings varchar(3))")
 
 
 # writing json restaurant data to database for ratings 
 
 for data in json_rest_data['yelp_data']:
 
-    sql_query = "INSERT INTO `Top 5 Restaurant Results by Location` (cities, #1, #2, #3, #4, #5, 'Average Rating`, `Average Price`) VALUES (?, ?, ?, ?, ?, ?, ?, ?)"
-    data = data['city']
+    sql_query = "INSERT INTO `Restaurants and Ratings` (cities, restaurants, ratings) VALUES (?, ?, ?)"
+    city = data['city']
     results = data['results'] # dictionary 
-    top5 = results['Top 3']
+    top3 = results['Top 3']
+    top3 = str(top3)
     ratings = results['Ratings']
+    ratings = str(ratings)
 
+    values = (city, top3, ratings)
+    cur.execute(sql_query, values)
+conn.commit()
+
+
+
+
+
+# creating table for cities with top 3 restaurants and prices 
+cur.execute("DROP TABLE IF EXISTS `Restaurants and Pricing`")
+cur.execute("CREATE TABLE `Restaurants and Pricing` (cities TEXT PRIMARY KEY, restaurants varchar(3), pricing varchar(3))")
 
 # writing json restaurant data to database for prices 
 
-
 for data in json_rest_data['yelp_data']:
 
-    sql_query = "INSERT INTO `Top 5 Restaurant Results by Location` (cities, #1, #2, #3, #4, #5, 'Average Rating`, `Average Price`) VALUES (?, ?, ?, ?, ?, ?, ?, ?)"
-    data = data['city']
+    sql_query = "INSERT INTO `Restaurants and Pricing` (cities, restaurants, pricing) VALUES (?, ?, ?)"
+    city = data['city']
     results = data['results'] # dictionary 
-    top5 = results['Top 3']
+    top3 = results['Top 3']
+    top3 = str(top3)
     prices = results['Prices']
+    prices = str(prices)
+
+    values = (city, top3, prices)
+    cur.execute(sql_query, values)
+conn.commit()
+
+
 
 
     
