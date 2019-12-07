f3f8d27fb6443873cc89303756157c38a8feba8c
Anna Gilhool <annakgil@umich.edu>
2019-12-07 15:39:43 -0500

    updated db-joins

--- db-joins.py
+++ db-joins.py
@@ -1,4 +1,5 @@
 import sqlite3
+import os
 
 def get_database(db_name):
     path = os.path.dirname(os.path.abspath(__file__))
@@ -7,8 +8,7 @@ def get_database(db_name):
     return cur, conn
 
 def db_join(cur, conn):
-    cur.execute('SELECT cities, restaurants, ratings, avg_rating')
-
+    cur.execute('SELECT cities, restaurants, ratings, avg_rating FROM `Restaurants and Ratings` INNER JOIN `Restaurants and Pricing` ON  `Restaurants and Ratings`.cities = `Restaurants and Pricing`.cities')
 
 
 cur, conn = get_database('final.db')
