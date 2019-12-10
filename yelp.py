8474bfef04cb0d5bb5c187ef37feecc56bd3e3e8
nfanaras <nfanaras@umich.edu>
2019-12-10 13:34:26 -0500

    removed comments

--- yelp.py
+++ yelp.py
@@ -8,17 +8,12 @@ headers = {'Authorization': 'Bearer %s' % api_key}
 url='https://api.yelp.com/v3/businesses/search'
 
 dict = {}
-yelp_data = []
+yelp_data = [] 
 dict['yelp_data'] = yelp_data
-# print(dict)
 
 city_list = ['New York', 'Los Angeles', 'Chicago', 'Houston', 'Philadelphia', 'Phoenix', 'San Antonio', 'San Diego', 'Dallas', 'San Jose', 'Austin', 'Indianapolis', 'Jacksonville', 'San Francisco', 'Columbus', 'Charlotte', 'Fort Worth', 'Detroit', 'El Paso', 'Memphis', 'Seattle', 'Denver', 'Washington', 'Boston', 'Nashville', 'Baltimore', 'Oklahoma City', 'Louisville', 'Portland', 'Las Vegas', 'Milwaukee', 'Albuquerque', 'Tucson', 'Fresno', 'Sacramento', 'Long Beach', 'Kansas City', 'Mesa', 'Virginia Beach', 'Atlanta', 'Colorado Springs', 'Omaha', 'Raleigh', 'Miami', 'Oakland', 'Minneapolis', 'Tulsa', 'Cleveland', 'Wichita', 'Arlington', 'New Orleans', 'Bakersfield', 'Tampa', 'Honolulu', 'Aurora', 'Anaheim', 'Santa Ana', 'St Louis', 'Riverside', 'Corpus Christi', 'Lexington','Pittsburgh','Anchorage','Stockton','Cincinnati','Saint Paul','Toledo','Greensboro','Newark','Plano','Henderson','Lincoln','Buffalo','Jersey City','Chula Vista','Fort Wayne','Orlando','St Petersburg','Chandler','Laredo','Norfolk','Durham','Madison','Lubbock','Irvine','Winston–Salem','Glendale','Garland','Hialeah','Reno','Chesapeake','Gilbert','Baton Rouge','Irving','Scottsdale','North Las Vegas','Fremont','Boise','Richmond','San Bernardino']
 
 for i in city_list:
-
-    # User input
-    # location = input("What city do you want to travel to: ")
-
 
     params = {'term':'restaurants','location': i}
 
@@ -29,27 +24,11 @@ for i in city_list:
 
     # Making a get request to the API
     req=requests.get(url, params=params, headers=headers)
-    
-    # proceed only if the status code is 200
-    # print('The status code is {}'.format(req.status_code))
 
     data = json.loads(req.text)
 
-    # #writing to a json file
-    # with open('yelp-restaurant-data.json', 'w', encoding='utf-8') as f:
-    #     json.dump(data, f, ensure_ascii=False, indent=4)
-
     # list of restaurants
     restaurants = data['businesses']
-
-    #restaurants with rating >= 4.5
-    # top_rest = []
-    # for rest in restaurants: 
-    #     if str(rest['rating']) == '4.5' or str(rest['rating']) == '5.0':
-    #         print(type(str(rest['rating'])))
-    #         top_rest.append([rest['name'], rest['rating']])
-
-    # print(top_rest)
 
     #top 5 rest results
     top_rest = []
@@ -62,7 +41,6 @@ for i in city_list:
             count += 1
         else:
             break
-    # print(top_rest)
 
     data_dict['results']['Top 3'] = names
 
@@ -70,19 +48,8 @@ for i in city_list:
     top_rest_ratings = []
     for rest in top_rest:
         top_rest_ratings.append(rest['rating'])
-    # print(top_rest_ratings)
 
     data_dict['results']['Ratings'] = top_rest_ratings
-
-
-    #average rating of top 3 results
-    # ratings = []
-    # for rest in top_rest_ratings:
-    #     ratings.append(rest[1])
-    # average_rating = sum(ratings)/len(ratings)
-    # print(average_rating)
-
-    # data_dict['results']['Average Rating'] = average_rating
 
     #gathering top 3 results prices
     top_rest_price = []
@@ -91,7 +58,6 @@ for i in city_list:
             top_rest_price.append(rest['price'])
         except:
             continue
-    # print(top_rest_price)
 
     #converting price to int
     prices = []
@@ -106,38 +72,13 @@ for i in city_list:
             rest = 4
         prices.append(rest)
         
-    # print(top_rest_price)
 
     data_dict['results']['Prices'] = prices
-
-    #averaging the price
-    # prices = []
-    # for rest in top_rest_price:
-    #     prices.append(rest[1])
-    # average_price = sum(prices)/len(prices)
-    # print(average_price)
-
-    # final_price = average_price
-
-    # if average_price <= float(1):
-    #     final_price = 'Inexpensive'
-    # elif average_price <= float(2):
-    #     final_price = "Moderate"
-    # elif average_price <= float(3):
-    #     final_price = "Pricey"
-    # elif average_price <= float(4):
-    #     final_price = "Ultra High-End"
-
-    # print(final_price)
-
-    # data_dict['results']['Average Price'] = final_price
 
     #making dictionary
     yelp_data.append(data_dict)
 
 dict['yelp_data'] = yelp_data
-# print(dict)
-
 
 # writing to json file
 
--- zomato-estab.py
+++ zomato-estab.py
@@ -6,27 +6,25 @@ cuisines = "https://developers.zomato.com/api/v2.1/establishments?city_id={}"
 header = {"User-agent": "curl/7.43.0", "Accept": "application/json", "user_key": "ec8d07cd9f4ba5a10333320eb29ebf2d"}
 
 city_list = ['New York', 'Los Angeles', 'Chicago', 'Houston', 'Philadelphia', 'Phoenix', 'San Antonio', 'San Diego', 'Dallas', 'San Jose', 'Austin', 'Indianapolis', 'Jacksonville', 'San Francisco', 'Columbus', 'Charlotte', 'Fort Worth', 'Detroit', 'El Paso', 'Memphis', 'Seattle', 'Denver', 'Washington', 'Boston', 'Nashville', 'Baltimore', 'Oklahoma City', 'Louisville', 'Portland', 'Las Vegas', 'Milwaukee', 'Albuquerque', 'Tucson', 'Fresno', 'Sacramento', 'Long Beach', 'Kansas City', 'Mesa', 'Virginia Beach', 'Atlanta', 'Colorado Springs', 'Omaha', 'Raleigh', 'Miami', 'Oakland', 'Minneapolis', 'Tulsa', 'Cleveland', 'Wichita', 'Arlington', 'New Orleans', 'Bakersfield', 'Tampa', 'Honolulu', 'Aurora', 'Anaheim', 'Santa Ana', 'St Louis', 'Riverside', 'Corpus Christi', 'Lexington','Pittsburgh','Anchorage','Stockton','Cincinnati','Saint Paul','Toledo','Greensboro','Newark','Plano','Henderson','Lincoln','Buffalo','Jersey City','Chula Vista','Fort Wayne','Orlando','St Petersburg','Chandler','Laredo','Norfolk','Durham','Madison','Lubbock','Irvine','Winston–Salem','Glendale','Garland','Hialeah','Reno','Chesapeake','Gilbert','Baton Rouge','Irving','Scottsdale','North Las Vegas','Fremont','Boise','Richmond','San Bernardino']
+# needed city ids for API request
 city_ids = [280, 281, 292, 277, 287, 301, 304, 302, 276, 10883, 278, 718, 574, 306, 1021, 303, 10978, 285, 1168, 1144, 216, 305, 283, 289, 1138, 787, 1051, 745, 286, 282, 1267, 964, 466, 514, 499, 10924, 856, 11187, 1228, 288, 529, 946, 6890, 291, 10773, 5570, 1045, 1033, 730, 11175, 290, 496, 604, 640, 10611, 10924, 484, 844, 4772, 1174, 742, 1081, 415, 508, 1030, 10646, 1042, 4148, 3976, 11003, 5852, 943, 1012, 3959, 11250, 703, 601, 577, 11188, 1177, 10500, 907, 1264, 1189, 5863, 1060, 10959, 10992, 291, 976, 8111, 11193, 757, 10995, 11190, 282, 10796, 664, 1219, 10811]
 
 dict = {}
 zomato_data = []
 dict['zomato_data'] = zomato_data
 
-for city_id, city in zip(city_ids, city_list):
+for city_id, city in zip(city_ids, city_list): # loops through both city list of names and city ids 
     response = requests.get(cuisines.format(city_id), headers=header)
     data_dict = {}
     data_dict['city'] = city
     data_dict['results'] = response.json()
     zomato_data.append(data_dict)
 
-print(zomato_data)
-
 #sorting data
 
 count = 0
 
 list_cuisines = []
-
 
 for item in zomato_data:
     try:
@@ -51,20 +49,6 @@ for item in l:
     name = item['establishment']['name']
     names.append(name)
 list_cuisines.append(names)
-
-
-# print(list_cuisines)
-
-# with open('list-cuisines.json', 'w', encoding='utf-8') as f:
-#     json.dump(list_cuisines, f, ensure_ascii=False, indent=2)
-
-# counts = []
-
-# for item in list_cuisines:
-#     print(item)
-#     counts.append(len(item))
-# print(counts)
-
 
 sorted_data = {}
 cuisines_by_city = []
